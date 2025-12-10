a4ae71e65cec325c5adb5c7f00351742fe24c16a
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2025-12-10 17:42:31 +0000

    Use tabs for Go code!

--- 2025-12-07-DevOpsHour-Go/examples/server/main.go
+++ 2025-12-07-DevOpsHour-Go/examples/server/main.go
@@ -6,10 +6,10 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %s!\n", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hello, %s!\n", r.URL.Path[1:])
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
